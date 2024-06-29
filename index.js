0f84963e61e9f6227f27818f853310c59b85fcef
yasir <yasir.abdiweli@student.moringa.com>
2024-06-29 19:27:18 +0300

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`; // Assuming short greeting is meant to be just the first name initial followed by an exclamation mark
